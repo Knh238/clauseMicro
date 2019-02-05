51d48e8b36a6ded6df4306dbf549cb056c4de42c
Kristin <kristinnharper@gmail.com>
2019-02-05 15:27:02 -0500

    removed unncessary module

--- server/db/database.js
+++ server/db/database.js
@@ -1,8 +1,5 @@
-const chalk = require('chalk')
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
-
-console.log(chalk.yellow('Opening database connection'))
 
 const db = new Sequelize(`postgres://localhost:5432/clauseMicroservice`, {
   logging: false
