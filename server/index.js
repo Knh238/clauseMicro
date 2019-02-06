e489a8d036fd64c005adaa703eb0d074de9dec27
Kristin <kristinnharper@gmail.com>
2019-02-05 22:25:20 -0500

    the version that works locally for both front and backend api requests. independent of the heroku deployment

--- server/index.js
+++ server/index.js
@@ -20,15 +20,15 @@ const createApp = () => {
 
   app.use(express.static(path.join(__dirname, '..', 'public')))
 
-  app.use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
-    } else {
-      next()
-    }
-  })
+  // app.use((req, res, next) => {
+  //   if (path.extname(req.path).length) {
+  //     const err = new Error('Not found')
+  //     err.status = 404
+  //     next(err)
+  //   } else {
+  //     next()
+  //   }
+  // })
 
   app.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public/index.html'))
